8e7eef8a06515f50a46f8024d950306fda52e755
Ivana Chingovska <ivana.chingovska@idiap.ch>
2012-12-20 13:11:49 +0100

    changed setup.pu to v1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='antispoofing.lbp',
-    version='1.0.0',
+    version='1.1.0',
     description='Texture (LBP) based counter-measures for the REPLAY-ATTACK database',
     url='http://pypi.python.org/pypi/antispoofing.lbp',
     license='GPLv3',
