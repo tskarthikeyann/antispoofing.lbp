957614a09f82dd34290bfc06f79daba4ad8a9b1d
Ivana Chingovska <ivana.chingovska@idiap.ch>
2013-10-04 19:27:39 +0200

    changed version, removed sources from buildout

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 # Ivana Chingovska <ivana.chingovska@idiap.ch>
 # Sun Jul  8 20:35:55 CEST 2012
-
+#
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 from setuptools import setup, find_packages
 
 # The only thing we do in this file is to call the setup() function with all
@@ -9,13 +22,14 @@ from setuptools import setup, find_packages
 setup(
 
     name='antispoofing.lbp',
-    version='1.2.0a0',
+    version='1.2.0',
     description='Texture (LBP) based counter-measures for the REPLAY-ATTACK database',
     url='http://pypi.python.org/pypi/antispoofing.lbp',
     license='GPLv3',
     author='Ivana Chingovska',
     author_email='ivana.chingovska@idiap.ch',
     long_description=open('README.rst').read(),
+    keywords='antispoofing texture, LBP, face spoofing attacks, face spoofing database, bob, xbob',
 
     # This line is required for any distutils based packaging.
     packages=find_packages(),
