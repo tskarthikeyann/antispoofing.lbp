75cd3e961d3a08fb3ecdedaf59901eb70ad50b5b
Ivana Chingovska <ivana.chingovska@idiap.ch>
2013-10-07 15:08:55 +0200

    fixed perf table generation

--- antispoofing/lbp/script/cmphistmodels.py
+++ antispoofing/lbp/script/cmphistmodels.py
@@ -12,44 +12,7 @@ import bob
 import numpy
 
 from antispoofing.utils.db import *
-
-def create_full_dataset(indir, objects):
-  """Creates a full dataset matrix out of all the specified files"""
-  dataset = None
-  for obj in objects:
-    filename = os.path.expanduser(obj.make_path(indir, '.hdf5'))
-    fvs = bob.io.load(filename)
-    if dataset is None:
-      dataset = fvs
-    else:
-      dataset = numpy.append(dataset, fvs, axis = 0)
-  return dataset
-
-def map_scores(indir, score_dir, objects, score_list):
-  """Maps frame scores to frames of the objects. Writes the scores for each frame in a file, NaN for invalid frames
-
-  Keyword parameters:
-
-  indir: the directory with the files with valid frames
-
-  score_dir: the directory where the score files are going to be written
-
-  objects: list of objects
-
-  score_list: list of scores for the given objects
-  """
-  num_scores = 0 # counter for how many valid frames have been processed so far in total of all the objects
-  for obj in objects:
-    filename = os.path.expanduser(obj.make_path(indir, '.hdf5'))
-    vf = bob.io.load(filename)
-    vf_indices = list(numpy.where(vf == 1)[0]) # indices of the valid frames of the object
-    nvf_indices = list(numpy.where(vf == 0)[0]) # indices of the invalid frames of the object
-    scores = numpy.ndarray((len(vf), 1), dtype='float64') 
-    scores[vf_indices] = score_list[num_scores:num_scores + len(vf_indices)] # set the scores of the valid frames
-    scores[nvf_indices] = numpy.NaN # set NaN for the scores of the invalid frames
-    num_scores += len(vf_indices) # increase the nu,ber of valid scores that have been already maped
-    obj.save(scores, score_dir, '.hdf5') # save the scores
-
+    
 def main():
 
   basedir = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
@@ -65,11 +28,10 @@ def main():
   parser.add_argument('-d', '--output-dir', metavar='DIR', type=str, dest='outputdir', default=OUTPUT_DIR, help='Base directory that will be used to save the results.')
   parser.add_argument('-s', '--score', dest='score', action='store_true', default=False, help='If set, the final classification scores of all the frames will be dumped in a file')
 
-  from .. import ml
-  from .. import spoof
-  from ..ml import perf
+  from .. import spoof, helpers
   from ..spoof import chi2
-
+  from ..helpers import score_manipulate as sm
+   
   #######
   # Database especific configuration
   #######
@@ -93,9 +55,9 @@ def main():
   process_test_real, process_test_attack = database.get_test_data()
 
   # create the full datasets from the file data
-  devel_real = create_full_dataset(args.inputdir, process_devel_real); devel_attack = create_full_dataset(args.inputdir, process_devel_attack); 
-  test_real = create_full_dataset(args.inputdir, process_test_real); test_attack = create_full_dataset(args.inputdir, process_test_attack); 
-  train_real = create_full_dataset(args.inputdir, process_train_real); train_attack = create_full_dataset(args.inputdir, process_train_attack); 
+  devel_real = sm.create_full_dataset(args.inputdir, process_devel_real); devel_attack = sm.create_full_dataset(args.inputdir, process_devel_attack); 
+  test_real = sm.create_full_dataset(args.inputdir, process_test_real); test_attack = sm.create_full_dataset(args.inputdir, process_test_attack); 
+  train_real = sm.create_full_dataset(args.inputdir, process_train_real); train_attack = sm.create_full_dataset(args.inputdir, process_train_attack); 
 
   print "Loading the models..."
   # loading the histogram models
@@ -118,21 +80,37 @@ def main():
   sc_train_realmodel = (sc_train_realmodel[0] * -1, sc_train_realmodel[1] * -1)
 
   if args.score: # save the scores in a file
-    vf_dir = os.path.join(args.inputdir, 'validframes') # input directory with the files with valid frames
     score_dir = os.path.join(args.outputdir, 'scores') # output directory for the socre files
-    map_scores(vf_dir, score_dir, process_devel_real, sc_devel_realmodel[0]) 
-    map_scores(vf_dir, score_dir, process_devel_attack, sc_devel_realmodel[1])
-    map_scores(vf_dir, score_dir, process_test_real, sc_test_realmodel[0])
-    map_scores(vf_dir, score_dir, process_test_attack, sc_test_realmodel[1])
-    map_scores(vf_dir, score_dir, process_train_real, sc_train_realmodel[0])
-    map_scores(vf_dir, score_dir, process_train_attack, sc_train_realmodel[1])
+    sm.map_scores(args.inputdir, score_dir, process_devel_real, sc_devel_realmodel[0]) 
+    sm.map_scores(args.inputdir, score_dir, process_devel_attack, sc_devel_realmodel[1])
+    sm.map_scores(args.inputdir, score_dir, process_test_real, sc_test_realmodel[0])
+    sm.map_scores(args.inputdir, score_dir, process_test_attack, sc_test_realmodel[1])
+    sm.map_scores(args.inputdir, score_dir, process_train_real, sc_train_realmodel[0])
+    sm.map_scores(args.inputdir, score_dir, process_train_attack, sc_train_realmodel[1])
 
-  perftable, eer_thres, mhter_thres = perf.performance_table(sc_test_realmodel, sc_devel_realmodel, "CHI-2 comparison, RESULTS")
-  print perftable 
+  # calculation of the error rates
+  thres = bob.measure.eer_threshold(sc_devel_realmodel[1].flatten(), sc_devel_realmodel[0].flatten())
+  dev_far, dev_frr = bob.measure.farfrr(sc_devel_realmodel[1].flatten(), sc_devel_realmodel[0].flatten(), thres)
+  test_far, test_frr = bob.measure.farfrr(sc_test_realmodel[1].flatten(), sc_test_realmodel[0].flatten(), thres)
+  
+  # writing results to a file
+  tbl = []
+  tbl.append(" ")
+  tbl.append(" threshold: %.4f" % thres)
+  tbl.append(" dev:  FAR %.2f%% (%d / %d) | FRR %.2f%% (%d / %d) | HTER %.2f%% " % \
+      (100*dev_far, int(round(dev_far*len(devel_attack))), len(devel_attack), 
+       100*dev_frr, int(round(dev_frr*len(devel_real))), len(devel_real),
+       50*(dev_far+dev_frr)))
+  tbl.append(" test: FAR %.2f%% (%d / %d) | FRR %.2f%% (%d / %d) | HTER %.2f%% " % \
+      (100*test_far, int(round(test_far*len(test_attack))), len(test_attack),
+       100*test_frr, int(round(test_frr*len(test_real))), len(test_real),
+       50*(test_far+test_frr)))
+  txt = ''.join([k+'\n' for k in tbl])
+  print txt
 
+  # write the results to a file 
   tf = open(os.path.join(args.outputdir, 'perf_table.txt'), 'w')
-  tf.write(perftable)
-  tf.close()  
-
+  tf.write(txt)
+  
 if __name__ == '__main__':
   main()
